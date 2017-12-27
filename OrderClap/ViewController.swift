6dfbfd26647d4fbad4bf3fc78075ed036ffe0e31
Joakim Rosdahl <joakim.Rosdahl@gmail.com>
2017-12-28 00:27:04 +0100

    Initial Commit

---
+++ OrderClap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OrderClap
+//
+//  Created by Joakim Rosdahl on 2017-12-28.
+//  Copyright © 2017 OrderClap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
